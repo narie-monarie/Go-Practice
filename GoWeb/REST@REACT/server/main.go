60836aad04312e01f1748ab1e0246763008bbd72
monari <jobmonari7@gmail.com>
2022-11-08 19:59:15 +0300

    adding again again

---
+++ GoWeb/REST@REACT/server/main.go
@@ -0,0 +1,35 @@
+package main
+
+import (
+	"log"
+
+	"github.com/gofiber/fiber/v2"
+)
+
+type Todo struct {
+	ID    int    `json:"id"`
+	Title string `json:"title"`
+	Done  bool   `json:"done"`
+	Body  string `json:"body"`
+}
+
+func main() {
+	app := fiber.New()
+
+	todos := []Todo{}
+	app.Get("/healthcheck", func(c *fiber.Ctx) error {
+		return c.SendString("OK")
+	})
+
+	app.Post("/api/todos", func(c *fiber.Ctx) error {
+		todo := &Todo{}
+		if err := c.BodyParser(todo); err != nil {
+			return err
+		}
+		todo.ID = len(todos) + 1
+		todos = append(todos, *todo)
+		return c.JSON(todos)
+	})
+
+	log.Fatal(app.Listen(":4000"))
+}
