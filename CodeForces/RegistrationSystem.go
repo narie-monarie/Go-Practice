062074f65a8ff8dc09d13c794279c5d42fedbeb7
monarie <jobmonari7@gmail.com>
2023-02-22 21:13:33 +0300

    eeeiiissshhh

--- CodeForces/RegistrationSystem.go
+++ CodeForces/RegistrationSystem.go
@@ -1,10 +1,27 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"strconv"
+)
 
 func main() {
-	var x int
+	var x, a int
+	var n, v string
 	fmt.Scan(&x)
 	m := make(map[string]bool, x)
 
+	for x != 0 {
+		fmt.Scan(&n)
+		_, ok := m[n]
+		a++
+		if ok {
+			v = n + strconv.Itoa(a)
+			fmt.Println(v)
+		} else {
+			fmt.Println("OK")
+		}
+		m[n] = true
+		x--
+	}
 }
