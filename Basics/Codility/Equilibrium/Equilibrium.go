e48142d793550262793d2931fa999dadd0fc01d9
monarie <jobmonari7@gmail.com>
2023-03-21 15:35:30 +0300

    Equilibrium.go

--- Basics/Codility/Equilibrium/Equilibrium.go
+++ Basics/Codility/Equilibrium/Equilibrium.go
@@ -1,6 +1,23 @@
 package main
 
+import "math"
+
 func main() {
-	x := []int{3, 1, 2, 4, 3}
+	x := []int64{3, 1, 2, 4, 3}
+	a := []int64{}
+	var sum, n int64
+	for i := 0; i < len(x); i++ {
+		min := x[i]
+		for j := i + 1; j < len(x); j++ {
+			sum += x[j]
+		}
+		n = int64(math.Abs(float64(sum - min)))
+		a = append(a, n)
+		sum = 0
+	}
+
+	for _, l := range a {
+		println(l)
+	}
 
 }
